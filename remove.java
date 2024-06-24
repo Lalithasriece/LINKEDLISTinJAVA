5bab549eb542594e2d25f6071314ce936c47a848
Lalithasriece <144588804+Lalithasriece@users.noreply.github.com>
2024-06-24 18:05:20 +0530

    remove

---
+++ remove.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+class remove{
+    public static void main(String args[]){
+        //
+LinkedList<String> colors=new LinkedList<>();
+colors.add("yellow");
+colors.add("green");
+colors.add("white");
+colors.add("pink");
+colors.add("blue");   
+System.out.println(colors);
+//remove using index
+colors.remove(1);
+System.out.println(colors);
+//remove using objects
+colors.remove("pink");
+System.out.println(colors);
+    }
+}
