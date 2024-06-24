325c851d54d6283381ec1946fea268637b6fa4cc
Lalithasriece <144588804+Lalithasriece@users.noreply.github.com>
2024-06-24 18:06:55 +0530

    size

---
+++ size.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class size{
+    public static void main(String args[]){
+        //
+LinkedList<String> colors=new LinkedList<>();
+colors.add("yellow");
+colors.add("green");
+colors.add("white");
+colors.add("pink");
+colors.add("blue");   
+System.out.println(colors);
+//size();
+System.out.println(colors.size());
+
+    }
+}
