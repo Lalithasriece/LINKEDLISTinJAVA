f861cbc85c0febe5a45dbfbcd28b0531fe885ccb
Lalithasriece <144588804+Lalithasriece@users.noreply.github.com>
2024-06-24 18:04:08 +0530

    GetLast

---
+++ GetLast.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class GetLast{
+    public static void main(String args[]){
+        //
+LinkedList<String> colors=new LinkedList<>();
+colors.add("yellow");
+colors.add("green");
+colors.add("white");
+colors.add("pink");
+colors.add("blue");   
+System.out.println(colors);
+//getLast()
+System.out.println(colors.getLast());
+    }
+}
