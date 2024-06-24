dd67926ef636a8f6e1589adc08b62d478c63bc71
Lalithasriece <144588804+Lalithasriece@users.noreply.github.com>
2024-06-24 18:06:03 +0530

    clear

---
+++ clear.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class clear{
+    public static void main(String args[]){
+        //
+LinkedList<String> colors=new LinkedList<>();
+colors.add("yellow");
+colors.add("green");
+colors.add("white");
+colors.add("pink");
+colors.add("blue");   
+System.out.println(colors);
+//clear()
+colors.clear();
+System.out.println(colors);
+    }
+}
