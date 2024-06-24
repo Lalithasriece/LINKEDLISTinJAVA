20672340fa02fb6d4108ada5abffab160d59cfe8
Lalithasriece <144588804+Lalithasriece@users.noreply.github.com>
2024-06-24 18:01:47 +0530

    RemoveLast

---
+++ RemoveLast.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class RemoveLast{
+    public static void main(String args[]){
+        //
+LinkedList<String> colors=new LinkedList<>();
+colors.add("yellow");
+colors.add("green");
+colors.add("white");
+colors.add("pink");
+colors.add("blue");   
+System.out.println(colors);
+//removeLast();
+
+colors.removeLast();
+System.out.println(colors);
+    }
+}
