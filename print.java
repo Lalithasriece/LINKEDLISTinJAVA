44844192d5f2862236eb9c33f2566f34346f7852
Lalithasriece <144588804+Lalithasriece@users.noreply.github.com>
2024-06-24 18:04:38 +0530

    Add files via upload

---
+++ print.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class print{
+    public static void main(String args[]){
+        //print 5 names in array using linked list
+LinkedList <String> colors=new LinkedList<>();
+colors.add("yellow");
+colors.add("green");
+colors.add("white");
+colors.add("pink");
+colors.add("blue");   
+System.out.println(colors);
+    }
+}
